493baf8063d1b0080b01e13e317aaf4192d29cbc
Katherin Nuñez <katitanuma@outlook.com>
2020-07-23 15:48:03 -0600

    Pruebas

--- src/test/java/com/timp/test/MDR/AcessarMDR.java
+++ src/test/java/com/timp/test/MDR/AcessarMDR.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.sap.timp.base.TestBaseKathy;
 import com.sap.timp.base.TestBaseSteven;
 import com.sap.timp.pageObjectModel.ADM.LoginTC;
 import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;

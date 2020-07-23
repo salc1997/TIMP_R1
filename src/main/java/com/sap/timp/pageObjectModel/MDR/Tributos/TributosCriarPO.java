f9db9ce90ddf8a03fd1a687dc8a582a27e82cfb4
Katherin Nuñez <katitanuma@outlook.com>
2020-07-23 15:48:18 -0600

    Revert "Pruebas"
    This reverts commit 493baf8063d1b0080b01e13e317aaf4192d29cbc.

--- src/main/java/com/sap/timp/pageObjectModel/MDR/Tributos/TributosCriarPO.java
+++ src/main/java/com/sap/timp/pageObjectModel/MDR/Tributos/TributosCriarPO.java
@@ -1,36 +1,7 @@
 package com.sap.timp.pageObjectModel.MDR.Tributos;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
 import com.sap.timp.base.TestBaseKathy;
 
 public class TributosCriarPO extends TestBaseKathy{
-	@FindBy(xpath = "//*[@id=\"main-content\"]/div/div/div/div/div/ul/li[6]/div[1]")
-	public WebElement brb;
-	
-	@FindBy(xpath = "//*[@id=\"tkb-right-arrow\"]")
-	public WebElement derecha;
-	
-	@FindBy(xpath = "//*[@id=\"right\"]/div/div[4]/div/div[2]/div/div[5]")
-	public WebElement btnSuccess;
-	
-	public TributosCriarPO() {
-		PageFactory.initElements(driver, this);
-	}
-	
-	public boolean Acceder() {
-		while(!brb.isDisplayed()) {
-			derecha.click();					
-		}
-		waitExpectElement(brb);
-	
-		brb.click();	
-		waitExpectElement(btnSuccess);
-		
-		boolean exito = btnSuccess.isDisplayed();
-		return exito;
-	}
 
 }

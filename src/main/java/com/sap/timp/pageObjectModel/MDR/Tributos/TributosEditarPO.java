278a6853afad9b2c4b4b8e1d6cc91cf4ae9ac435
Katherin Nuñez <katitanuma@outlook.com>
2020-07-24 03:43:35 -0600

    Avances EditarTributos

--- src/main/java/com/sap/timp/pageObjectModel/MDR/Tributos/TributosEditarPO.java
+++ src/main/java/com/sap/timp/pageObjectModel/MDR/Tributos/TributosEditarPO.java
@@ -1,7 +1,29 @@
 package com.sap.timp.pageObjectModel.MDR.Tributos;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.sap.timp.base.TestBaseKathy;
 
 public class TributosEditarPO extends TestBaseKathy{
-
+	
+	@FindBy(xpath = "//li/div/span[text()=\"Centralização\"]")
+	public WebElement centralização;
+	
+	@FindBy(xpath = "//li/div/span[text()=\"Tributos\"]")
+	public WebElement tributos;
+	
+	public TributosEditarPO() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void tributosEditar() {
+		centralização.click();
+		sleep(1000);
+		tributos.click();
+		attributeToBeXpath("//div[contains(@class,\"tbody\")]", "class", "tbody hasShowHide");
+		sleep(2000);
+		
+	}
 }

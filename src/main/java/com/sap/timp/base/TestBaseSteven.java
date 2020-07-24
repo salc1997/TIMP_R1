0f1703b06b09262bd06fbeccc0cbede15526e585
Steven Lezama <lezamasteve@gmail.com>
2020-07-24 13:46:54 -0600

    Modificação AcessarMDR

--- src/main/java/com/sap/timp/base/TestBaseSteven.java
+++ src/main/java/com/sap/timp/base/TestBaseSteven.java
@@ -49,7 +49,7 @@ public class TestBaseSteven{
 		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get(td1);
+		driver.get(tc2);
 		return driver;
 
 	}

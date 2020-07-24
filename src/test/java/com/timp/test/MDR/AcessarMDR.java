8215e726677ae63e0cb4ac20ad56a3612575f45b
Katherin Nuñez <katitanuma@outlook.com>
2020-07-23 21:33:33 -0600

    Revert "pruebas cambio testbase"
    This reverts commit e0497540d1a3b774c5ac4299446efaaebd0b761d.

--- src/test/java/com/timp/test/MDR/AcessarMDR.java
+++ src/test/java/com/timp/test/MDR/AcessarMDR.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.sap.timp.base.TestBaseKathy;
 import com.sap.timp.base.TestBaseSteven;
 import com.sap.timp.pageObjectModel.ADM.LoginTC;
 import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;

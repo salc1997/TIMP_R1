0d07df74dc97dcda3e89b320d7c6b4abb904fbce
Steven Lezama <lezamasteve@gmail.com>
2020-07-25 09:25:07 -0600

    Municipio visualizar e cadastro responsavel

---
+++ src/main/java/com/sap/timp/pageObjectModel/MDR/CadastroDerex/CadastroResponsavel/ResponsavelMovimentoVisualizarPO.java
@@ -0,0 +1,84 @@
+package com.sap.timp.pageObjectModel.MDR.CadastroDerex.CadastroResponsavel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.sap.timp.base.TestBaseSteven;
+
+public class ResponsavelMovimentoVisualizarPO extends TestBaseSteven{
+	
+	@FindBy(xpath = "//li/div/span[text()=\"Cadastro DEREX\"]")
+	public WebElement derex;
+	
+	@FindBy(xpath = "//li[@identifier=\"accordion-item-responsibleDerex\"]")
+	public WebElement responsavelO;
+	
+	@FindBy(xpath = "//div/div/span[text()=\"Id\"]")
+	public WebElement idC;
+
+	@FindBy(xpath = "//div[contains(@class,\"icon-right\")][2]")
+	public WebElement siguiente;
+
+	@FindBy(xpath = "//li/span[text()=\"Visualizar\"]")
+	public WebElement visualizar;
+	
+	@FindBy(xpath = "//*[@id=\"left-content\"]/div/ul/li[4]")
+	public WebElement atividades1;
+
+	@FindBy(xpath = "//*[@id=\"left-content\"]/div/ul/li[4]/ul/li[3]")
+	public WebElement atividadesO1;
+
+	@FindBy(xpath = "//*[@id=\"left-content\"]/div/ul/li[3]")
+	public WebElement atividades2;
+
+	@FindBy(xpath = "//*[@id=\"left-content\"]/div/ul/li[3]/ul/li[3]")
+	public WebElement atividadesO2;
+	
+	@FindBy(xpath = "//div[contains(@class,\"tbody\")]/div[contains(@class,\"tr\") and @data-id]/div/div")
+	public WebElement menu;
+	
+	public ResponsavelMovimentoVisualizarPO() {
+
+		PageFactory.initElements(driver, this);
+	}
+
+	public String visualizar() {
+		
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+		sleep(1000);
+		derex.click();
+		sleep(1000);
+		responsavelO.click();
+		sleep(2000);
+		
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+
+		sleep(2000);
+
+		siguiente.click();
+
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+		sleep(2000);
+
+		idC.click();
+		sleep(1000);
+		dobleClickElement(idC);
+
+		
+		menu.click();
+		sleep(1000);
+		visualizar.click();
+		
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+		sleep(2000);
+
+		String sucesso = driver.findElement(By.xpath("//table[@class=\"edit\"]")).getAttribute("class");
+
+		return sucesso;
+
+	}
+
+}

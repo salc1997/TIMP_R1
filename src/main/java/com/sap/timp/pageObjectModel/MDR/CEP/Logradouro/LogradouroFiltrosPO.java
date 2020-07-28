d10870ff8430a29295ea33f7e8bad42666f02be8
Katherin Nuñez <katitanuma@outlook.com>
2020-07-28 16:39:43 -0600

    LogradouroFiltros

---
+++ src/main/java/com/sap/timp/pageObjectModel/MDR/CEP/Logradouro/LogradouroFiltrosPO.java
@@ -0,0 +1,16 @@
+package com.sap.timp.pageObjectModel.MDR.CEP.Logradouro;
+
+import org.openqa.selenium.support.PageFactory;
+
+import com.sap.timp.base.TestBaseKathy;
+
+public class LogradouroFiltrosPO extends TestBaseKathy{
+	
+	public LogradouroFiltrosPO() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void logradouroFiltros() {
+		
+	}
+}
---
+++ src/test/java/com/timp/test/MDR/CEP/Logradouro/LogradouroFiltros.java
@@ -0,0 +1,44 @@
+package com.timp.test.MDR.CEP.Logradouro;
+
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseKathy;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
+import com.sap.timp.pageObjectModel.MDR.CEP.Logradouro.LogradouroFiltrosPO;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class LogradouroFiltros extends TestBaseKathy{
+	LoginTC loginTC;
+  	AcessarMDRPO acessarMDRPO;
+  	LogradouroFiltrosPO logradouroFiltrosPO;
+  
+  	@BeforeClass
+  	public void beforeClass() {
+	  	driver = initializationK();
+	  	loginTC = new LoginTC();
+	  	acessarMDRPO = new AcessarMDRPO();
+	  	logradouroFiltrosPO = new LogradouroFiltrosPO();
+  	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+	  
+	@Test(priority = 0)
+	public void login() {
+		loginTC.login();
+	}
+
+	@Test(priority = 1)
+	public void acessarMDR() {
+		acessarMDRPO.acessarMDR();
+	}
+	
+	@Test(priority = 2)
+	public void logradouroFiltros() {
+		logradouroFiltrosPO.logradouroFiltros();
+	}
+}

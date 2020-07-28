3691a186231efd635fde56bd3914298fceec8514
Eliel Alves <eliel.leandro@alltaxplatform.com>
2020-07-28 13:14:43 -0300

    Sequencia Exluir. Sequencia Visualizar.

---
+++ src/main/java/com/sap/timp/pageObjectModel/MDR/SequenciaAcesso/SequenciaVisualizarPO.java
@@ -0,0 +1,82 @@
+package com.sap.timp.pageObjectModel.MDR.SequenciaAcesso;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.sap.timp.base.TestBaseEliel;
+
+public class SequenciaVisualizarPO extends TestBaseEliel {
+	
+	@FindBy(xpath = "//li/div/span[text()=\"Sequência de Acesso\"]")
+	public WebElement sequencia;
+		
+		
+	@FindBy(xpath = "//*[@id=\"list\"]/div/div[1]/div/div[2]/div/div[3]/div/span[1]")
+	public WebElement idC;
+	
+	@FindBy(xpath = "//div[@class=\"tr first\" and @data-id][1]/div[3]/div")
+	public WebElement idR;
+	
+
+	@FindBy(xpath = "//*[@id=\"list\"]/div/div[2]/div/div[7]")
+	public WebElement setafinal;
+	
+	@FindBy(xpath = "//div[@class=\"tr first\" and @data-id][1]/div[2]/label/span")
+	public WebElement opcao;
+	
+	@FindBy(xpath = "//div[@class=\"tr first\" and @data-id][1]/div[1]/div")
+	public WebElement engrenagem;
+	
+	@FindBy(xpath = "//*[@id=\"list\"]/div/div[1]/div/div[3]/div[3]/div[1]/div/div[2]/ul/li[3]/span[2]")
+	public WebElement visualizar;
+	
+	
+	public SequenciaVisualizarPO() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	public String visualizar() {
+		
+		
+		//Actions action = new Actions(driver);
+
+		
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+		sequencia.click();
+		sleep(1000);
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+		
+		//Pega o ultimo id
+		setafinal.click();
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+		idC.click();
+		dobleClickElement(idC);
+		sleep(2000);
+		
+		//String id = idR.getText();
+		//sleep(2000);
+		//System.out.println(id);
+		
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+			
+		opcao.click();
+		
+		engrenagem.click();
+		
+		visualizar.click();
+		
+		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
+		
+		String sucesso = driver.findElement(By.xpath("//table[@class=\"edit\"]")).getAttribute("class");
+		System.out.println(sucesso);	
+		return sucesso;
+		
+	}
+	
+
+
+}

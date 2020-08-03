3f9774a805efb8bd4852bb5a8c5a683866771d15
Steven Lezama <lezamasteve@gmail.com>
2020-08-03 15:04:42 -0600

    teste em paralelo

--- src/test/java/com/timp/test/BRB/testNg/NovoRelatorio.java
+++ src/test/java/com/timp/test/BRB/testNg/NovoRelatorio.java
@@ -6,40 +6,26 @@ import static org.testng.Assert.assertTrue;
 
 import java.util.ArrayList;
 
-import javax.swing.text.html.HTML;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.sap.timp.base.TestBaseParalelo2;
 import com.sap.timp.base.TestBaseSteven;
 import com.sap.timp.pageObjectModel.ADM.LoginTC;
 import com.sap.timp.pageObjectModel.BRB.AcessarBrbPO;
-import com.sap.timp.pageObjectModel.BRB.NovoRelatorioPO;
+import com.sap.timp.pageObjectModel.BRB.prueba.NovoRelatorioPO;
 
-public class NovoRelatorio extends TestBaseSteven{
+public class NovoRelatorio extends TestBaseParalelo2{
 	
 	LoginTC loginTc;
 	AcessarBrbPO accesarBrbPO;
 	NovoRelatorioPO novoRelatorioPO;
 
-
 	@BeforeClass
 	public void beforeClass() {
-		driver = initialization();
+		driver= initialization();
 		loginTc = new LoginTC();
 		accesarBrbPO = new AcessarBrbPO();
 		novoRelatorioPO = new NovoRelatorioPO();
@@ -47,7 +33,7 @@ public class NovoRelatorio extends TestBaseSteven{
 	
 	@AfterClass
 	public void afterClass(){
-		driver.close();
+		//driver.close();
 	}
 	
 	@Test(priority = 0)
@@ -74,6 +60,7 @@ public class NovoRelatorio extends TestBaseSteven{
 		
 		assertTrue(sucesso, novoRelatorio);
 		sleep(4000);
+		
 		String texto = driver.findElement(By.xpath("//*[@id=\"right\"]/div/div[4]/div/div[1]/div/div[3]/div[1]/div[7]/div")).getText();
 		assertTrue(texto.contains("Prueba Automatizada"), "The names don't match");
 			

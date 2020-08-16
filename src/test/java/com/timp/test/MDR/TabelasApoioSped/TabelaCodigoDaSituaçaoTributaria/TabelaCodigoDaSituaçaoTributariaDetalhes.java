82ef3568deef276a0e87a06ceaad5025627ee740
Eliel Alves <eliel.leandro@alltaxplatform.com>
2020-08-16 11:56:57 -0300

    Tabela Codigo da situaçao tributaria

---
+++ src/test/java/com/timp/test/MDR/TabelasApoioSped/TabelaCodigoDaSituaçaoTributaria/TabelaCodigoDaSituaçaoTributariaDetalhes.java
@@ -0,0 +1,56 @@
+package com.timp.test.MDR.TabelasApoioSped.TabelaCodigoDaSituaçaoTributaria;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseEliel;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
+import com.sap.timp.pageObjectModel.MDR.TabelasApoioSped.TabelaCodigoDaSituaçaoTributaria.TabelaCodigoDaSituaçaoTributariaDetalhesPO;
+
+
+public class TabelaCodigoDaSituaçaoTributariaDetalhes extends TestBaseEliel {
+  
+	LoginTC loginTC;
+	AcessarMDRPO acessarMDRPO;
+	TabelaCodigoDaSituaçaoTributariaDetalhesPO tabelaCodigoDaSituaçaoTributariaDetalhesPO;
+	
+	@BeforeClass
+	public void beforeClass() {
+
+		driver = initializationE();
+		loginTC = new LoginTC();
+		acessarMDRPO = new AcessarMDRPO();
+		tabelaCodigoDaSituaçaoTributariaDetalhesPO = new TabelaCodigoDaSituaçaoTributariaDetalhesPO();
+	}
+	/*
+	 * @AfterClass public void afterClass(){ driver.close(); }
+	 */
+
+	@Test(priority = 0)
+	public void login() {
+		loginTC.login();
+
+	}
+
+	@Test(priority = 1)
+	public void acessarMDR() {
+
+		acessarMDRPO.acessarMDR();
+
+	}
+
+	@Test(priority = 2)
+	public void Visualizar() {
+
+		
+		String sucesso = tabelaCodigoDaSituaçaoTributariaDetalhesPO.detalhes();
+		
+		 assertEquals(sucesso, "edit", visualizaçar);
+	}	
+				
+	
+	
+}

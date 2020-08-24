703dc82cbdce96ac0755472b6bb937e91a34e01f
Eliel Alves <eliel.leandro@alltaxplatform.com>
2020-08-24 10:25:00 -0300

    Diversos

---
+++ src/test/java/com/timp/test/MDR/TabelasApoioSped/DeParaLivroApuraçaoXSpedFiscal/DeParaLivroApuraçaoXSpedFiscalExcluir.java
@@ -0,0 +1,56 @@
+package com.timp.test.MDR.TabelasApoioSped.DeParaLivroApuraçaoXSpedFiscal;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseEliel;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
+import com.sap.timp.pageObjectModel.MDR.TabelasApoioSped.DeParaLivroApuraçaoXSpedFiscal.DeParaLivroApuraçaoXSpedFiscalExcluirPO;
+
+public class DeParaLivroApuraçaoXSpedFiscalExcluir extends TestBaseEliel {
+	
+	
+	
+	
+	LoginTC loginTC;
+	AcessarMDRPO acessarMDRPO;
+	DeParaLivroApuraçaoXSpedFiscalExcluirPO deParaLivroApuraçaoXSpedFiscalExcluirPO;
+
+	@BeforeClass
+	public void beforeClass() {
+
+		driver = initializationE();
+		loginTC = new LoginTC();
+		acessarMDRPO = new AcessarMDRPO();
+		deParaLivroApuraçaoXSpedFiscalExcluirPO = new DeParaLivroApuraçaoXSpedFiscalExcluirPO();
+	}
+	/*
+	 * @AfterClass public void afterClass(){ driver.close(); }
+	 */
+
+	@Test(priority = 0)
+	public void login() {
+		loginTC.login();
+
+	}
+
+	@Test(priority = 1)
+	public void acessarMDR() {
+
+		acessarMDRPO.acessarMDR();
+
+	}
+
+	@Test(priority = 2)
+	public void excluir() {
+
+		
+		boolean sucesso = deParaLivroApuraçaoXSpedFiscalExcluirPO.excluir();
+		assertTrue(sucesso, Eliminado);
+		
+	}			
+
+}

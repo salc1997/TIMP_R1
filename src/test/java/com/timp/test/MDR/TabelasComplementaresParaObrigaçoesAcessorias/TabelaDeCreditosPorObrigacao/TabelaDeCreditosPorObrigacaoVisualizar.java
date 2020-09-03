05d800e719b9679d850beec6d03d1381aaffdb75
Eliel Alves <eliel.leandro@alltaxplatform.com>
2020-09-03 10:50:23 -0300

    Diversos

---
+++ src/test/java/com/timp/test/MDR/TabelasComplementaresParaObrigaçoesAcessorias/TabelaDeCreditosPorObrigacao/TabelaDeCreditosPorObrigacaoVisualizar.java
@@ -0,0 +1,57 @@
+package com.timp.test.MDR.TabelasComplementaresParaObrigaçoesAcessorias.TabelaDeCreditosPorObrigacao;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseEliel;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
+import com.sap.timp.pageObjectModel.MDR.TabelasComplementaresParaObrigaçoesAcessorias.TabelaDeCreditosPorObrigacao.TabelaDeCreditosPorObrigacaoVisualizarPO;
+
+public class TabelaDeCreditosPorObrigacaoVisualizar extends TestBaseEliel {
+	
+	LoginTC loginTC;
+	AcessarMDRPO acessarMDRPO;
+	TabelaDeCreditosPorObrigacaoVisualizarPO tabelaDeCreditosPorObrigacaoVisualizarPO;
+	@BeforeClass
+	public void beforeClass() {
+
+		driver = initializationE();
+		loginTC = new LoginTC();
+		acessarMDRPO = new AcessarMDRPO();
+		tabelaDeCreditosPorObrigacaoVisualizarPO = new TabelaDeCreditosPorObrigacaoVisualizarPO();
+	}
+	/*
+	 * @AfterClass public void afterClass(){ driver.close(); }
+	 */
+
+	@Test(priority = 0)
+	public void login() {
+		loginTC.login();
+
+	}
+
+	@Test(priority = 1)
+	public void acessarMDR() {
+
+		acessarMDRPO.acessarMDR();
+
+	}
+
+	@Test(priority = 2)
+	public void Visualizar() {
+
+		ArrayList<Boolean> sucesso = tabelaDeCreditosPorObrigacaoVisualizarPO.visualizar();
+		
+		for (int i = 0; i < sucesso.size(); i++) {
+			
+			assertTrue(sucesso.get(i), visualizaçar);
+			
+		}
+	}			
+	
+}

8bfe983d7495dcd3b85042d1a392aa26ac881d49
Eliel Alves <eliel.leandro@alltaxplatform.com>
2020-09-08 18:25:41 -0300

    Diversos

---
+++ src/test/java/com/timp/test/MDR/TabelasComplementaresParaObrigaçoesAcessorias/CodigoDeOutrasSaidas/CodigoDeOutrasSaidasVisualizar.java
@@ -0,0 +1,63 @@
+package com.timp.test.MDR.TabelasComplementaresParaObrigaçoesAcessorias.CodigoDeOutrasSaidas;
+
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseEliel;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
+import com.sap.timp.pageObjectModel.MDR.TabelasComplementaresParaObrigaçoesAcessorias.CodigoDeOutrasSaidas.CodigoDeOutrasSaidasVisualizarPO;
+
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.AfterClass;
+
+public class CodigoDeOutrasSaidasVisualizar extends TestBaseEliel {
+	
+
+	LoginTC loginTC;
+	AcessarMDRPO acessarMDRPO;
+	CodigoDeOutrasSaidasVisualizarPO codigoDeOutrasSaidasVisualizarPO;
+	@BeforeClass
+	public void beforeClass() {
+
+		driver = initializationE();
+		loginTC = new LoginTC();
+		acessarMDRPO = new AcessarMDRPO();
+		codigoDeOutrasSaidasVisualizarPO = new CodigoDeOutrasSaidasVisualizarPO();
+	}
+	/*
+	 * @AfterClass public void afterClass(){ driver.close(); }
+	 */
+
+	@Test(priority = 0)
+	public void login() {
+		loginTC.login();
+
+	}
+
+	@Test(priority = 1)
+	public void acessarMDR() {
+
+		acessarMDRPO.acessarMDR();
+
+	}
+
+	@Test(priority = 2)
+	public void Visualizar() {
+
+		ArrayList<Boolean> sucesso = codigoDeOutrasSaidasVisualizarPO.visualizar();
+		
+		for (int i = 0; i < sucesso.size(); i++) {
+			
+			assertTrue(sucesso.get(i), visualizaçar);
+			
+		}
+	}			
+	
+ 
+
+}

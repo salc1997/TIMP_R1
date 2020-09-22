5ccff8176ce12f034e26f8a5b0523d1e85e2fde4
Steven Lezama <lezamasteve@gmail.com>
2020-09-22 16:58:38 -0600

    testbase modification

--- src/main/java/com/sap/timp/base/TestBaseSteven.java
+++ src/main/java/com/sap/timp/base/TestBaseSteven.java
@@ -44,9 +44,9 @@ public class TestBaseSteven {
 
 		WebDriver driver;
 		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
-		//ChromeOptions chromeOptions = new ChromeOptions();
-		//chromeOptions.setBinary("C:\\Program Files (x86)\\Google\\Update\\Download\\{8A69D345-D564-463C-AFF1-A69D9E530F96}\\84.0.4147.89\\84.0.4147.89_chrome_installer.exe");
-		//driver = new ChromeDriver(chromeOptions);
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+		driver = new ChromeDriver(chromeOptions);
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(td1);

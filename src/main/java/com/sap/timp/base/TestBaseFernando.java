ca9c0290acf8cae45a7c18651bea49a9d9e6eb2c
Steven Lezama <lezamasteve@gmail.com>
2020-10-09 14:19:59 -0600

    Revert "Ticket do sprint 14 do dia 09/10/2020"
    This reverts commit a56fcae4d4d53e8b61c937e1cad01ab53a9f016f.

--- src/main/java/com/sap/timp/base/TestBaseFernando.java
+++ src/main/java/com/sap/timp/base/TestBaseFernando.java
@@ -12,7 +12,7 @@ public class TestBaseFernando extends TestBaseSteven {
 		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get(td1);
+		driver.get(tp1);
 		return driver;
 	}
 

ef7f7daa566fc8dfe0af4b1b207ebd1a670e1a2d
Eliel Alves <eliel.leandro@alltaxplatform.com>
2020-10-19 17:57:36 -0300

    Sprint 15 - Tickets do dia 19/10/2020

---
+++ src/test/java/com/timp/test/MDR/Siscoserv/PaisMoeda/PaisMoedaVisualizar.java
@@ -0,0 +1,61 @@
+package com.timp.test.MDR.Siscoserv.PaisMoeda;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseEliel;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
+import com.sap.timp.pageObjectModel.MDR.Siscoserv.PaisMoeda.PaisMoedaVisualizarPO;
+
+public class PaisMoedaVisualizar extends TestBaseEliel{
+ 
+	LoginTC loginTC;
+	AcessarMDRPO acessarMDRPO;
+	PaisMoedaVisualizarPO paisMoedaVisualizarPO;
+
+	@BeforeClass
+	public void beforeClass() {
+
+		driver = initializationE();
+		loginTC = new LoginTC();
+		acessarMDRPO = new AcessarMDRPO();
+		paisMoedaVisualizarPO = new PaisMoedaVisualizarPO();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+
+	@Test(priority = 0)
+	public void login() {
+		loginTC.login();
+
+	}
+
+	@Test(priority = 1)
+	public void acessarMDR() {
+
+		acessarMDRPO.acessarMDR();
+
+	}
+
+	@Test(priority = 2)
+	public void Visualizar() {
+
+		ArrayList<Boolean> sucesso = paisMoedaVisualizarPO.visualizar();
+
+		for (int i = 0; i < sucesso.size(); i++) {
+
+			assertTrue(sucesso.get(i), visualizaçar);
+
+		}
+	}
+	
+}

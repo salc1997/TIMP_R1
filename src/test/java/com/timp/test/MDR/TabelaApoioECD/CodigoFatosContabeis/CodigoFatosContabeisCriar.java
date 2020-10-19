3027d743eec322b787f196baad7dfb800356cc53
Fernando Sanchez <fernando.sanchez@agilesolutions.com>
2020-10-19 16:19:29 -0600

    Prueba git Fernando Sanchez

---
+++ src/test/java/com/timp/test/MDR/TabelaApoioECD/CodigoFatosContabeis/CodigoFatosContabeisCriar.java
@@ -0,0 +1,53 @@
+package com.timp.test.MDR.TabelaApoioECD.CodigoFatosContabeis;
+
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseFernando;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
+import com.sap.timp.pageObjectModel.MDR.TabelaApoioECD.CodigoFatosContabeis.CodigoFatosContabeisCriarPO;
+
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.AfterClass;
+
+public class CodigoFatosContabeisCriar extends TestBaseFernando {
+	LoginTC loginTC;
+	AcessarMDRPO acesarMDRPO;
+	CodigoFatosContabeisCriarPO ecd;
+
+	public CodigoFatosContabeisCriar() {
+		super();
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = initializationF();
+		loginTC = new LoginTC();
+		acesarMDRPO = new AcessarMDRPO();
+		ecd = new CodigoFatosContabeisCriarPO();
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+	@Test(priority = 0)
+	public void ingresar() {
+		loginTC.login();
+	}
+
+	@Test(priority = 2)
+	public void mdrEntrar() {
+		boolean sucesso = acesarMDRPO.acessarMDR();
+		assertTrue(sucesso, "The element is not present");
+	}
+
+	@Test(priority = 3)
+	public void criar() {
+		boolean sucesso = ecd.criar();
+		assertTrue(sucesso, "There is an error...");
+	}
+}

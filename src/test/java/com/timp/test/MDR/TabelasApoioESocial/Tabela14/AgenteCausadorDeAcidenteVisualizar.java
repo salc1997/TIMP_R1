e47ff0660a086bcd1124803b36f11d9f5fcb9e97
Steven Lezama <lezamasteve@gmail.com>
2020-10-19 16:17:00 -0600

    tickets do sprint15 do dia 19/10/2020

---
+++ src/test/java/com/timp/test/MDR/TabelasApoioESocial/Tabela14/AgenteCausadorDeAcidenteVisualizar.java
@@ -0,0 +1,67 @@
+package com.timp.test.MDR.TabelasApoioESocial.Tabela14;
+
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseEliel;
+import com.sap.timp.base.TestBaseSteven;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
+import com.sap.timp.pageObjectModel.MDR.SequenciaAcesso.SequenciaCriarPO;
+import com.sap.timp.pageObjectModel.MDR.TabelasApoioESocial.Tabela14.AgenteCausadorDeAcidenteVisualizarPO;
+import com.sap.timp.pageObjectModel.MDR.TabelasApoioESocial.Tabela26.MotivosCesBeneficiosCriarPO;
+import com.sap.timp.pageObjectModel.MDR.TabelasApoioESocial.Tabela26.MotivosCesBeneficiosVisualizarPO;
+
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.AfterClass;
+
+public class AgenteCausadorDeAcidenteVisualizar extends TestBaseSteven {
+
+	LoginTC loginTC;
+	AcessarMDRPO acessarMDRPO;
+	AgenteCausadorDeAcidenteVisualizarPO agenteCausadorDeAcidenteVisualizarPO;
+
+	@BeforeClass
+	public void beforeClass() {
+
+		driver = initialization();
+		loginTC = new LoginTC();
+		acessarMDRPO = new AcessarMDRPO();
+		agenteCausadorDeAcidenteVisualizarPO = new AgenteCausadorDeAcidenteVisualizarPO();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+
+	@Test(priority = 0)
+	public void login() {
+		loginTC.login();
+
+	}
+
+	@Test(priority = 1)
+	public void acessarMDR() {
+
+		acessarMDRPO.acessarMDR();
+
+	}
+
+	@Test(priority = 2)
+	public void visualizar() {
+
+		ArrayList<Boolean> sucesso = agenteCausadorDeAcidenteVisualizarPO.visualizar();
+
+		for (int i = 0; i < sucesso.size(); i++) {
+			assertTrue(sucesso.get(i), visualizaçar);
+		}
+
+	}
+
+}

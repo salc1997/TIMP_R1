ccd80c1b6dbd9850217d7dd54a14b749d7ac4b9b
Steven Lezama <lezamasteve@gmail.com>
2020-10-21 10:23:54 -0600

    test base massiel

---
+++ src/main/java/com/sap/timp/base/TestBaseMassiel.java
@@ -0,0 +1,19 @@
+package com.sap.timp.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBaseMassiel extends TestBaseSteven {
+
+	TestBaseSteven testeBaseSteven = new TestBaseSteven();
+
+	public WebDriver initializationM() {
+
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(tc2);
+		return driver;
+	}
+
+}

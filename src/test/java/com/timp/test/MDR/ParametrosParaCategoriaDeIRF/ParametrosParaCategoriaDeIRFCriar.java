a5ac8f6889e2f7c9c966bb7ba96803d889b58890
Steven Lezama <lezamasteve@gmail.com>
2020-11-24 14:28:39 -0600

    modificacion

--- src/test/java/com/timp/test/MDR/ParametrosParaCategoriaDeIRF/ParametrosParaCategoriaDeIRFCriar.java
+++ src/test/java/com/timp/test/MDR/ParametrosParaCategoriaDeIRF/ParametrosParaCategoriaDeIRFCriar.java
@@ -5,16 +5,10 @@ import static org.testng.Assert.assertTrue;
 import org.junit.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import com.sap.timp.base.TestBaseEliel;
-
-import com.sap.timp.base.TestBaseSteven;
 import com.sap.timp.pageObjectModel.ADM.LoginTC;
 import com.sap.timp.pageObjectModel.MDR.AcessarMDRPO;
-import com.sap.timp.pageObjectModel.MDR.ParametrosOficializacaoLivros.ParametrosOficializacaoLivrosCriarPO;
 import com.sap.timp.pageObjectModel.MDR.ParametrosParaCategoriaDeIRF.ParametrosParaCategoriaDeIRFCriarPO;
-import com.sap.timp.pageObjectModel.MDR.TabelasComplementaresParaObrigaçoesAcessorias.AgrupadorDeCFOP.AgrupadorDeCFOPCriarPO;
-import com.sap.timp.pageObjectModel.MDR.TabelasComplementaresParaObrigaçoesAcessorias.TabelaOutrosDebitos.TabelasOutrosDebitosCriarPO;
+
 
 
 

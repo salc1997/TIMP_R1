21c7d56cf771c9cc7869e27a10a67cd4ae0f5c03
Eliel Alves <eliel.leandro@alltaxplatform.com>
2020-12-16 10:20:13 -0300

    Arquivos 16/12/2020

--- src/main/java/com/sap/timp/base/TestBaseCristhian.java
+++ src/main/java/com/sap/timp/base/TestBaseCristhian.java
@@ -19,7 +19,7 @@ public class TestBaseCristhian extends TestBaseSteven{
 		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get(tq1);
+		driver.get(tc2);
 		return driver;
 	}
 	
--- src/main/java/com/sap/timp/base/TestBaseMassiel.java
+++ src/main/java/com/sap/timp/base/TestBaseMassiel.java
@@ -25,7 +25,7 @@ public class TestBaseMassiel extends TestBaseSteven {
 
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get(tq1);
+		driver.get(tc2);
 
 	   return driver;
 	}

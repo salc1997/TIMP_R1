0155655560abf111d4f80b71426f42eb1c2a0a04
YessyVasquez <yessy.vasquez@agilesolutions.com>
2021-01-09 08:54:29 -0600

    comit  8-01-2021

--- src/test/java/com/timp/test/TAA/Ajustes/AjustesEditar.java
+++ src/test/java/com/timp/test/TAA/Ajustes/AjustesEditar.java
@@ -5,36 +5,36 @@ import org.testng.annotations.Test;
 import com.sap.timp.base.TestBaseMassiel;
 import com.sap.timp.pageObjectModel.ADM.LoginTC;
 import com.sap.timp.pageObjectModel.TAA.AcessarTAAPO;
-import com.sap.timp.pageObjectModel.TAA.Ajuste.AjustedCriarPO;
+import com.sap.timp.pageObjectModel.TAA.Ajuste.AjustesEditarPO;
 
 import org.testng.annotations.BeforeClass;
 
 import static org.testng.Assert.assertTrue;
 
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 
-public class AjustesCriar extends TestBaseMassiel{
+public class AjustesEditar extends TestBaseMassiel{
 	LoginTC loginTC;
 	AcessarTAAPO acessarTAAPO;
-	AjustedCriarPO ajustedCriarPO;
- 
+	AjustesEditarPO ajustesEditarPO;
+	
   @BeforeClass
   public void beforeClass() {
 	  driver = initializationM();
 		loginTC = new LoginTC();
 		acessarTAAPO  = new AcessarTAAPO();
-		ajustedCriarPO = new AjustedCriarPO();
+		ajustesEditarPO = new AjustesEditarPO();
   }
 
-  @AfterClass
-  public void afterClass() {
+  @AfterTest
+  public void afterTest() {
   }
-
+  
   @Test(priority = 0)
 	public void ingresar() {
 		loginTC.login();
 	}
-  
+
   @Test(priority = 1)
 	public void TAAEntrar() {
 	  acessarTAAPO.acessarTAA();
@@ -42,11 +42,11 @@ public class AjustesCriar extends TestBaseMassiel{
 	}
   
   @Test(priority = 2)
- 	public void ajustesCriar() {
-	  boolean sucesso = ajustedCriarPO.criar();
+	public void editar() {
+	  boolean sucesso = ajustesEditarPO.editar();
 		assertTrue(sucesso, Criar);
+	  
 
- 	}
-
+	}
 
 }

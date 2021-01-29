3fb8cf5bea2e32a1eb4dd9daa8b4bdc6d5a5b3c9
Eliel Alves <eliel.leandro@alltaxplatform.com>
2021-01-29 10:23:02 -0300

    Master

--- src/main/java/com/sap/timp/base/TestBaseCristhian.java
+++ src/main/java/com/sap/timp/base/TestBaseCristhian.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-
+ 
 public class TestBaseCristhian extends TestBaseSteven{
  
 	TestBaseSteven testeBaseSteven = new TestBaseSteven();

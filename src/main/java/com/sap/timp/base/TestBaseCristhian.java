b1392475c77a6eb39830748e1fe97984513a0723
Steven Lezama <lezamasteve@gmail.com>
2021-02-09 10:08:50 -0600

    arquivos restatesn

--- src/main/java/com/sap/timp/base/TestBaseCristhian.java
+++ src/main/java/com/sap/timp/base/TestBaseCristhian.java
@@ -36,7 +36,7 @@ public class TestBaseCristhian extends TestBaseSteven{
         
 		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
-		driver.get(tc2);
+		driver.get(tq1);
 		
 		return driver;
 	}

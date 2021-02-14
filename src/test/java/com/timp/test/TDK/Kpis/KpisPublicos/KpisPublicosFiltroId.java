56b94a5e8c44c308b2219936a53ac2b238aa7608
YessyVasquez <yessy.vasquez@agilesolutions.com>
2021-02-14 12:40:57 -0600

    tickets de jueves

---
+++ src/test/java/com/timp/test/TDK/Kpis/KpisPublicos/KpisPublicosFiltroId.java
@@ -0,0 +1,54 @@
+package com.timp.test.TDK.Kpis.KpisPublicos;
+
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseMassiel;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.TDK.AcessarTDKPO;
+import com.sap.timp.pageObjectModel.TDK.Kpis.KpisPublicos.KpisPublicosFiltroIdPO;
+
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.AfterClass;
+
+public class KpisPublicosFiltroId extends TestBaseMassiel{
+	LoginTC loginTC;
+	AcessarTDKPO acessarTDKPO;
+	KpisPublicosFiltroIdPO kpisPublicosFiltroIdPO;
+	
+  @BeforeClass
+  public void beforeClass() {
+	  driver = initializationM();
+		loginTC = new LoginTC();
+		acessarTDKPO = new AcessarTDKPO();
+		kpisPublicosFiltroIdPO = new KpisPublicosFiltroIdPO();
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @Test(priority = 0)
+	public void login() {
+		loginTC.login();
+	}
+
+	@Test(priority = 1)
+	public void acessarTDK() {
+		acessarTDKPO.acessarTDK();
+	}
+	
+	@Test(priority = 2)
+	public void filtro() {
+		
+		ArrayList<Boolean> sucesso = kpisPublicosFiltroIdPO.FiltroID();
+		for (int i = 0; i < sucesso.size(); i++) {
+			assertTrue(sucesso.get(i),Filtros);
+		}
+		
+	}
+}

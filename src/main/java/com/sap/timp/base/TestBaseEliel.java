7ea6636adeab9972bd6bdcc16efd5ec711eb11e0
Eliel Alves <eliel.leandro@alltaxplatform.com>
2021-02-15 11:00:03 -0300

    Atualização

--- src/main/java/com/sap/timp/base/TestBaseEliel.java
+++ src/main/java/com/sap/timp/base/TestBaseEliel.java
@@ -22,7 +22,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBaseEliel extends TestBaseSteven {
-
+ 
 	TestBaseSteven testeBaseSteven = new TestBaseSteven();
 
 	public WebDriver initializationE() {

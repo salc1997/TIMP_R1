8c440ad149b4d7ff62f3f2d067213ff1b4fffd08
Fernando Sanchez <fs_sanchez97@outlook.es>
2021-03-02 11:59:48 -0600

    Tickets TDK

---
+++ src/test/java/com/timp/test/TDK/Execucoes/ExecucoesPublicas/ExecucoesFiltroPorIDMasLimpiarFiltros.java
@@ -0,0 +1,52 @@
+package com.timp.test.TDK.Execucoes.ExecucoesPublicas;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sap.timp.base.TestBaseFernando;
+import com.sap.timp.pageObjectModel.ADM.LoginTC;
+import com.sap.timp.pageObjectModel.TDK.AcessarTDKPO;
+import com.sap.timp.pageObjectModel.TDK.Execucoes.ExecucoesPublicas.ExecucoesFiltroPorIDMasLimpiarFiltrosPO;
+
+public class ExecucoesFiltroPorIDMasLimpiarFiltros extends TestBaseFernando{
+	LoginTC loginTC;
+	AcessarTDKPO acessarTDKPO;
+	ExecucoesFiltroPorIDMasLimpiarFiltrosPO execucoesFiltroPorIDMasLimpiarFiltrosPO;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = initializationF();
+		loginTC = new LoginTC();
+		acessarTDKPO = new AcessarTDKPO();
+		execucoesFiltroPorIDMasLimpiarFiltrosPO = new ExecucoesFiltroPorIDMasLimpiarFiltrosPO();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+
+	@Test(priority = 0)
+	public void login() {
+		loginTC.login();
+	}
+
+	@Test(priority = 1)
+	public void acessarTDK() {
+		acessarTDKPO.acessarTDK();
+	}
+
+	@Test(priority = 2)
+	public void detalhes() {
+		ArrayList<Boolean> sucesso = execucoesFiltroPorIDMasLimpiarFiltrosPO.filtroPorID();
+
+		for (int i = 0; i < sucesso.size(); i++) {
+			assertTrue(sucesso.get(i), Filtros);
+		}
+	}
+}

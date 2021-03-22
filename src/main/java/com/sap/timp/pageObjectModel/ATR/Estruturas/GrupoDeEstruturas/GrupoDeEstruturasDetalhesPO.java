88c0f641b2221e8d4a44e03886408135406238fb
YessyVasquez <yessy.vasquez@agilesolutions.com>
2021-03-22 08:14:15 -0600

    cambios de insert ,  obter

--- src/main/java/com/sap/timp/pageObjectModel/ATR/Estruturas/GrupoDeEstruturas/GrupoDeEstruturasDetalhesPO.java
+++ src/main/java/com/sap/timp/pageObjectModel/ATR/Estruturas/GrupoDeEstruturas/GrupoDeEstruturasDetalhesPO.java
@@ -111,7 +111,7 @@ public class GrupoDeEstruturasDetalhesPO extends TestBaseEliel {
 		sleep(2000);
 		
 		//pega o ultimo id que foi gerado no criar
-		String idRegistro1 = idObter1();
+		String idRegistro1 = idObter("GrupoDeEstruturasCriar");
 		
 		WebElement menu = driver.findElement(By.xpath("//div[@data-id=\""+idRegistro+"\"]/div[1]/div"));
 		WebElement ver = driver.findElement(By.xpath("//div[@data-id=\""+idRegistro+"\"]/div[1]/div/div[2]/ul/li/span[text()=\"Ver\"]"));
--- src/main/java/com/sap/timp/pageObjectModel/ATR/Estruturas/GrupoDeEstruturas/GrupoDeEstruturasVerPO.java
+++ src/main/java/com/sap/timp/pageObjectModel/ATR/Estruturas/GrupoDeEstruturas/GrupoDeEstruturasVerPO.java
@@ -109,7 +109,7 @@ public class GrupoDeEstruturasVerPO extends TestBaseEliel{
 		sleep(2000);
 		
 		//pega o ultimo id que foi gerado no criar
-		String idRegistro1 = idObter1();
+		String idRegistro1 = idObter("GrupoDeEstruturasCriar");
 		
 		WebElement menu1 = driver.findElement(By.xpath("//div[@data-id=\""+idRegistro1+"\"]/div[1]/div"));
 		WebElement editar = driver.findElement(By.xpath("//div[@data-id=\""+idRegistro1+"\"]/div[1]/div/div[2]/ul/li/span[text()=\"Editar\"]"));

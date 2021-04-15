789cf6f32bc8469696d6d616eaf150c3b5e6f38c
Kenssy Medina <kenssy.medina@agilesolutions.com>
2021-04-15 09:50:06 -0600

    TestBase cambio a TC2

--- src/main/java/com/sap/timp/base/TestBaseMassiel.java
+++ src/main/java/com/sap/timp/base/TestBaseMassiel.java
@@ -50,7 +50,7 @@ public class TestBaseMassiel extends TestBaseSteven {
         
 		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
-		driver.get(tp1);
+		driver.get(tc2);
 
 	    return driver;
 	}

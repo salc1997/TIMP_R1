1e9c537ec6e714bb0b526c0aec724241e68e7749
YessyVasquez <yessy.vasquez@agilesolutions.com>
2021-04-17 23:43:06 -0600

    cambios mdr

--- src/test/java/com/timp/test/MDR/LivrosFiscais/ParametrizaçãoDoLivroOficial/ParametrizaçãoDoLivroOficialVisualizar.java
+++ src/test/java/com/timp/test/MDR/LivrosFiscais/ParametrizaçãoDoLivroOficial/ParametrizaçãoDoLivroOficialVisualizar.java
@@ -30,7 +30,7 @@ public class ParametrizaçãoDoLivroOficialVisualizar extends TestBaseMassiel{
 
   @AfterClass
   public void afterClass() {
-	  driver.close();
+	//  driver.close();
   }
 
 	@Test()

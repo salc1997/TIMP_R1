01151a357079cf31467c68d31f80708e8ea4bf65
YessyVasquez <yessy.vasquez@agilesolutions.com>
2021-08-19 00:06:19 -0600

    Cambios Compu rosada

--- src/test/java/com/timp/test/TCC/CentralizaçãoDeTributos/CentralizaçãoDeTributosFiltroXID.java
+++ src/test/java/com/timp/test/TCC/CentralizaçãoDeTributos/CentralizaçãoDeTributosFiltroXID.java
@@ -29,21 +29,12 @@ public class CentralizaçãoDeTributosFiltroXID extends TestBaseSteven {
 		driver.close();
 	}
 
-	@Test(priority = 0)
-	public void login() {
-		loginTC.login();
 
-	}
 
-	@Test(priority = 1)
-	public void acessarTCC() {
-
-		acessarTCCPO.acessarTCC();
-
-	}
-
-	@Test(priority = 2)
+	@Test
 	public void Filtro() {
+		loginTC.login();
+		acessarTCCPO.acessarTCC();
 
 		centralizaçãoDeTributosFiltroXIDPO.Filtros();
 

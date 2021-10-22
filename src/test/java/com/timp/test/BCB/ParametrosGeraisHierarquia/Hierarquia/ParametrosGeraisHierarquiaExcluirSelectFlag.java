1a4f5b101252fdec78d3cf0f05ef64354f373441
YessyVasquez <yessy.vasquez@agilesolutions.com>
2021-10-22 09:11:11 -0600

    cambios

--- src/test/java/com/timp/test/BCB/ParametrosGeraisHierarquia/Hierarquia/ParametrosGeraisHierarquiaExcluirSelectFlag.java
+++ src/test/java/com/timp/test/BCB/ParametrosGeraisHierarquia/Hierarquia/ParametrosGeraisHierarquiaExcluirSelectFlag.java
@@ -30,7 +30,7 @@ public class ParametrosGeraisHierarquiaExcluirSelectFlag extends TestBaseKenssy{
 
 	@AfterClass
 	public void afterClass() {
-		driver.close();
+		//driver.close();
 	}
 
 	@Test()

a64293c9ed2166975c3e353f3987bb33a9d5244c
Yessy Vasquez <yessy.vasquez@agilesolutions.com>
2023-06-15 13:11:00 +0000

    cambios al rodar TA para PB

--- src/main/java/com/sap/timp/pageObjectModel/BCB/OrigemBFB/HierarquiaConfiguracao/OrigenBFBHierarquiaConfiguracaoTodasAsFuncionalidadesPO.java
+++ src/main/java/com/sap/timp/pageObjectModel/BCB/OrigemBFB/HierarquiaConfiguracao/OrigenBFBHierarquiaConfiguracaoTodasAsFuncionalidadesPO.java
@@ -627,7 +627,7 @@ public class OrigenBFBHierarquiaConfiguracaoTodasAsFuncionalidadesPO extends Tes
 		gravarC.click();
 		sleep(3000);
 		invisibilityOfElement("//div[@class=\"overlay loader dark\"]");
-		sleep(8000);
+		sleep(12000);
 	
 		biblioteca.click();
 		sleep(5000);
